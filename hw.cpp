b2655c012c3226504ce38a2fe86d9a2182b0554b
Utkarsh Kumar <utkarsh18e@iitg.ac.in>
2021-07-08 18:22:34 +0530

    adding my template

---
+++ hw.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define pb push_back
+#define mii map<int, int>
+#define mll map<ll, ll>
+#define pii pair<int, int>
+#define pic pair<int, char>
+#define pdd pair<double, double>
+#define pll pair<long, long>
+#define vvi vector<vector<int>>
+#define ull unsigned long long int
+#define ll long long int
+
+
+int main()
+{
+    return 0;
+}
